0fc2cafe099449401834ad5daafa5d72c02d05ac
Jishnu Prakash K P <jishnuprakash.kp@gmail.com>
2022-05-13 12:33:44 +0100

    data, code and readme

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # KNN from scratch
  Implementing K-Nearest neighbours for multi class classification from scratch
+
+## About
+K-Nearest neighbhour is the world's simplest algorithm for multi-class classification. In this project, KNN is implemented using numpy. You can run and compare KNN with Cosine similarity, Euclidean distance and Manhattan distance. 
+
+## Requirements
+python 3, numpy (pip install numpy), matplotlib (pip install matplotlib)
+
+## Folder Structure
+Please make sure, the files knn.py and iris.csv are in the same location/folder. 
+
+## How to run the code?
+After installing python and numpy, it is very easy to run the knn.py file. 
+- Open the code in any Python IDE and click on run
+or
+- Open command prompt from project folder and run "python /knn.py"
+
+
+# Code in Detail
+## Importing libraries
+Importing numpy library for array operations and numpy random module for shuffling the data
+Set random seed to constant, to get same accuracy everytime
+
+## Reading the data
+readData Method is used to read iris.csv file and convert to numpy array.
+
+## KNN
+Class KNN represents the K-nearest neighbhour algorithm. it has following methods, distance, 
+predict, and accuracy.
+
+## distance
+distance method is used for computing Cosine similarity/ Euclidean distance/ Manhattan distance of input test data and train data.
+
+## predict
+predict method is used to execute K-NN algorithm.
+
+## accuracy
+accuracy method is used to compute accuracy from predictions and target variables.
---
+++ knn.py
@@ -0,0 +1,116 @@
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load train and test data
+def readData(data):
+    """
+    Reads data and converts to numpy arrays 
+    """
+    dat = np.array([i.split(',') for i in open(data,"r").read().splitlines()])
+    dat = dat[1:].astype(float)
+    return dat
+
+data = readData("iris.csv")
+np.random.seed(3)
+np.random.shuffle(data)
+
+train_data = np.array(data[:100])
+test_data = np.array(data[100:])
+
+#%%
+# K-Nearest Neighbour for Multi-class classification
+class KNN():
+    """
+    K- Nearest Neighbour
+
+    Class variable: -
+    data - train data
+    dist - distance used for classification
+
+    Methods: -
+    actScore, actFunction, updateRule, and training
+    """
+
+    def __init__(self, data, dist='cos'):
+        """
+        initialise KNN class
+        """
+        self.data = data
+        self.dist = dist
+
+    def distance(self, X):
+        """
+        Compute distance
+        cos, euclidean, manhattan
+        """
+        distances = []
+        if self.dist == 'cos':
+            for i in self.data:
+                score = np.dot(i[0:4],X) / (np.linalg.norm(i[0:4]) * np.linalg.norm(X))
+                distances.append(score)
+
+        elif self.dist == 'euclidean':
+            for i in self.data:
+                score = np.linalg.norm(i[0:4] - X)
+                distances.append(score)
+        
+        elif self.dist == 'manhattan':
+            for i in self.data:
+                score = sum(abs(val1-val2) for val1, val2 in zip(i[0:4], X))
+                distances.append(score)
+        return distances
+
+    def predict(self, x, k):
+        """
+        Predict x with given value of k 
+        """
+        # Get labels from train data
+        labels = self.data[:, 4]
+        #Calculate distance with each data point
+        distance = self.distance(x)
+        #Find index of distances
+        if self.dist == 'cos':
+            distanceIndex = np.argsort(distance)[::-1]
+        else:
+            distanceIndex = np.argsort(distance)
+        #Fetch item from labels
+        preds = [labels[i] for i in distanceIndex[:k]]
+        #Find count of predictions
+        counts = [preds.count(i) for i in preds]
+        #Return prediction with maximum count
+        return preds[np.argmax(counts)]
+
+    def accuracy(self, test_data, K):
+        """
+        Computes accuracy of predictions and target labels
+        """
+        pred = [self.predict(i[0:4], K) for i in test_data] 
+        #computing accuracy
+        acc = sum(1 for x,y in zip(test_data[:,4], pred) if x == y) / float(len(test_data)) 
+        return round(acc*100, 2)
+
+def plotAccuracy(kRange, accuracies):
+    # Plot the results
+    plt.plot(kRange, accuracies, 'b')
+    plt.xlabel("Parameter k")
+    plt.ylabel("Accuracy of k-NN")
+    plt.title("k-NN accuracy versus k")
+    plt.show()
+
+#%%
+
+kRange = range(1,100,2)
+# Compute accuracies
+dist = ['cos', 'euclidean', 'manhattan']
+
+for i in dist:
+    knn = KNN(train_data, dist=i)
+    accuracies = [knn.accuracy(test_data, i) for i in kRange]
+    print("\nDistance used:", i)
+    # Find the best k for the current validation dataset
+    print("Best k: ", kRange[np.argmax(accuracies)], "with accuracy:", max(accuracies))
+    plotAccuracy(kRange, accuracies)
+
+
+# %%
